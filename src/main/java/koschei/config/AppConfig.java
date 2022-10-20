82b3822df3f0ae0ec8f6839bca0e808e87f1ad7c
ilyatr1321 <ilyatr1321@gmail.com>
2022-10-20 20:18:21 +0300

    PP_2_1_4_spring-types-of-wiring_ilyatr1321

--- src/main/java/koschei/config/AppConfig.java
+++ src/main/java/koschei/config/AppConfig.java
@@ -1,6 +1,8 @@
 package koschei.config;
 
+import koschei.models.Egg6;
 import koschei.models.Island2;
+import koschei.models.Needle7;
 import koschei.models.Wood3;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -9,6 +11,11 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @ComponentScan(basePackages = "koschei")
 public class AppConfig {
+
+    @Bean
+    public static Egg6 getEgg6(Needle7 needle) {
+        return new Egg6(needle);
+    }
 
     @Bean
     public static Island2 getIsland(Wood3 wood) {
